166ba44e7088cc019c0a02f48b0da409cc722ebd
logic-fabric <lowic@logic-fabric.fr>
2021-08-19 15:00:21 +0200

    feature: add React-Router and GlobalStyle

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
 
-import reportWebVitals from './reportWebVitals';
+import { Error404 } from "./pages/Error404/Error404.jsx";
+import { Home } from "./pages/Home/Home";
+import { Login } from "./pages/Login/Login";
+import { UserProfile } from "./pages/UserProfile/UserProfile";
 
-ReactDOM.render(
-  <React.StrictMode>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import reportWebVitals from "./reportWebVitals";
+
+function initApp() {
+  ReactDOM.render(
+    <React.StrictMode>
+      <GlobalStyle />
+
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+
+          <Route path="/login">
+            <Login />
+          </Route>
+
+          <Route path="/profile">
+            <UserProfile />
+          </Route>
+
+          <Route>
+            <Error404 />
+          </Route>
+        </Switch>
+      </Router>
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+}
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+
+    scroll-behavior: smooth;
+  }
+`;
+
+initApp();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ src/pages/UserProfile/UserProfile.jsx
@@ -0,0 +1,7 @@
+export function UserProfile() {
+  return (
+    <div>
+      <h1>UserProfile page</h1>
+    </div>
+  );
+}
