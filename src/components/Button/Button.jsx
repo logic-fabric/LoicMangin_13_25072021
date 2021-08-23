a99b16718911165bb6313d56220eae047afb4548
logic-fabric <lowic@logic-fabric.fr>
2021-08-23 10:32:10 +0200

    refactor: Button as an independant component

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+import { styleVar } from "../../utils/styleVariables";
+
+export function Button({ type, text, callToAction }) {
+  return (
+    <StyledButton type={type} callToAction={callToAction}>
+      {text}
+    </StyledButton>
+  );
+}
+
+const StyledButton = styled.button`
+  display: block;
+
+  margin: 2rem 0 0 0;
+  padding: 0.75rem;
+  border: none;
+  border-radius: 0.25rem;
+
+  color: white;
+  font-size: 1.2rem;
+  font-weight: 700;
+
+  background: ${styleVar.primary600};
+
+  cursor: pointer;
+  transition: all 300ms;
+
+  ${(props) =>
+    props.callToAction
+      ? `
+        width: 100%;`
+      : ``}
+
+  &:hover {
+    background: ${styleVar.primary700};
+  }
+`;
--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,28 +1,29 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
+import { Button } from "../Button/Button";
 import { styleVar } from "../../utils/styleVariables";
 
 export function LoginForm() {
   return (
     <form>
       <LabeledInput>
-        <label for="username">Username</label>
+        <label  htmlFor="username">Username</label>
         <input type="text" id="username" />
       </LabeledInput>
 
       <LabeledInput>
-        <label for="password">Password</label>
+        <label htmlFor="password">Password</label>
         <input type="password" id="password" />
       </LabeledInput>
 
       <LabeledCheckbox>
         <input type="checkbox" id="remember-me" />
-        <label for="remember-me">Remember me</label>
+        <label htmlFor="remember-me">Remember me</label>
       </LabeledCheckbox>
 
       <SignInLink to="/profile">
-        <SignInButton>Sign In</SignInButton>
+        <Button type="button" text="Sign In" callToAction={true} />
       </SignInLink>
     </form>
   );
@@ -58,31 +59,8 @@ const LabeledCheckbox = styled.div`
   label {
     margin: 0 0 0 0.5rem;
   }
-`
+`;
 
 const SignInLink = styled(Link)`
   text-decoration: none;
-`;
-
-const SignInButton = styled.button`
-  display: block;
-
-  width: 100%;
-  margin: 2rem 0 0 0;
-  padding: 0.75rem;
-  border: none;
-  border-radius: 0.25rem;
-
-  color: white;
-  font-size: 1.2rem;
-  font-weight: 700;
-
-  background: ${styleVar.primary600};
-
-  cursor: pointer;
-  transition: all 300ms;
-
-  &:hover {
-    background: ${styleVar.primary700};
-  }
 `;
