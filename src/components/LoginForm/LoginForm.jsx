6425fa57c15ef146bff91f1a4a5917b693e396e1
logic-fabric <lowic@logic-fabric.fr>
2021-09-24 10:34:07 +0200

    feature: ask for JWT authentication token using /user/login POST to API

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
@@ -5,16 +6,98 @@ import { Button } from "../Button/Button";
 import { styleVar } from "../../utils/style/styleVariables";
 
 export function LoginForm() {
+  const [userInfos, setUserInfos] = useState({
+    email: "",
+    password: "",
+    token: "",
+  });
+
+  const [askForAuthentication, setAskForAuthentication] = useState(false);
+  const [submitError, setSubmitError] = useState(false);
+
+  const handleUserEmailInputChange = (event) => {
+    setUserInfos({ ...userInfos, email: event.target.value });
+
+    setSubmitError(false);
+  };
+
+  const handlePasswordInputChange = (event) => {
+    setUserInfos({ ...userInfos, password: event.target.value });
+
+    setSubmitError(false);
+  };
+
+  console.log("userInfos =", userInfos);
+
+  const handleLoginFormSubmit = (event) => {
+    event.preventDefault();
+
+    setAskForAuthentication(true);
+  };
+
+  useEffect(() => {
+    if (askForAuthentication) {
+      const url = "http://localhost:3001/api/v1/user/login";
+
+      const loginPayload = {
+        email: userInfos.email,
+        password: userInfos.password,
+      };
+
+      const requestOptions = {
+        method: "POST",
+        body: JSON.stringify(loginPayload),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+
+      // Send POST request:
+      fetch(url, requestOptions)
+        .then((response) => response.json())
+        .then((json) => {
+          setUserInfos({
+            ...userInfos,
+            token: json.body.token,
+          });
+
+          console.log("/user/login response to POST:", json);
+        })
+        .catch((error) => {
+          setSubmitError(true);
+
+          setUserInfos({
+            ...userInfos,
+            token: "",
+          });
+
+          console.error(
+            `An error has occured while posting to /user/login: ${error}`
+          );
+        });
+
+      setAskForAuthentication(false);
+    }
+  }, [userInfos, askForAuthentication]);
+
   return (
-    <form>
+    <form onSubmit={handleLoginFormSubmit}>
       <LabeledInput>
-        <label  htmlFor="username">Username</label>
-        <input type="text" id="username" />
+        <label htmlFor="username">E-mail</label>
+        <input
+          type="text"
+          id="username"
+          onChange={handleUserEmailInputChange}
+        />
       </LabeledInput>
 
       <LabeledInput>
         <label htmlFor="password">Password</label>
-        <input type="password" id="password" />
+        <input
+          type="password"
+          id="password"
+          onChange={handlePasswordInputChange}
+        />
       </LabeledInput>
 
       <LabeledCheckbox>
@@ -22,9 +105,7 @@ export function LoginForm() {
         <label htmlFor="remember-me">Remember me</label>
       </LabeledCheckbox>
 
-      <SignInLink to="/profile">
-        <Button type="button" text="Sign In" callToAction={true} />
-      </SignInLink>
+      <Button type="submit" text="Sign In" callToAction={true} />
     </form>
   );
 }
