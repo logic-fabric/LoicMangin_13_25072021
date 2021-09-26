80d3656dca261875f86979e5f49164f99ec8ee80
logic-fabric <lowic@logic-fabric.fr>
2021-09-26 08:58:58 +0200

    feature: Header MainNavList now changes when user is logged in

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,27 +1,45 @@
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+
 import styled from "styled-components";
 
 import { styleVar } from "../../utils/style/styleVariables";
 import ArgentBankLogo from "../../assets/argent-bank-logo.png";
 
 export function Header() {
+  const token = useSelector((state) => state.user.token);
+
   return (
     <HeaderContainer>
       <MainNav>
+        <Link to="/">
+          <LogoBox>
+            <img src={ArgentBankLogo} alt="Argent Bank" />
+          </LogoBox>
+        </Link>
+
+        <h1 className="sr-only">Argent Bank</h1>
+
         <MainNavList>
           <li>
-            <HeaderLink className="logo-link" to="/">
-              <LogoBox>
-                <img src={ArgentBankLogo} alt="Argent Bank" />
-              </LogoBox>
-            </HeaderLink>
-            <h1 className="sr-only">Argent Bank</h1>
+            <i className="fa fa-user-circle"></i>
+
+            {token ? (
+              <HeaderLink to="/profile">[ USER NAME ]</HeaderLink>
+            ) : (
+              <HeaderLink to="/login">Sign In</HeaderLink>
+            )}
           </li>
 
-          <li>
-            <i className="fa fa-user-circle"></i>
-            <HeaderLink to="/login">Sign In</HeaderLink>
-          </li>
+          {token ? (
+            <li>
+              <i className="fas fa-sign-out-alt"></i>
+
+              <HeaderLink to="/">Sign Out</HeaderLink>
+            </li>
+          ) : (
+            ""
+          )}
         </MainNavList>
       </MainNav>
     </HeaderContainer>
@@ -33,15 +51,21 @@ const HeaderContainer = styled.header`
 `;
 
 const MainNav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
   ${styleVar.largeWrapper}
 `;
 
 const MainNavList = styled.ul`
   display: flex;
-  justify-content: space-between;
-  align-items: center;
 
   padding: 0.25rem 1rem;
+
+  li {
+    margin: 0 0.5rem;
+  }
 
   li i {
     margin: 0 0.5rem 0 0;
